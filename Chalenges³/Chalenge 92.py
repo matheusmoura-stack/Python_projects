635062ada2cbd238467bd8e9fe8b6c094e6733ff
matheusmoura-stack <matheus.moura.g.s@gmail.com>
2022-01-02 18:33:10 -0300

    novo1

--- Chalenges³/Chalenge 92.py
+++ Chalenges³/Chalenge 92.py
@@ -1,13 +1,22 @@
-
+from datetime import date
 perfil = dict()
-perfil['nome'] = str(input('Nome: '))
+perfil['nome'] = str(input('Nome: ')).capitalize()
 perfil['nascimento'] = int(input(f'Ano de nascimento de {perfil["nome"]}: '))
-perfil['Carteira'] = int(input(f'Carteira de trabalho de {perfil["nome"]} (0 não tem): '))
-if perfil['Carteira'] != 0:
-    perfil['ano'] = int(input(f'Ano de contratação: '))
+year = date.today().year
+idade = year - perfil['nascimento']
+perfil.copy()
+del(perfil['nascimento'])
+perfil['idade'] = idade
+perfil['Ctps'] = int(input(f'Carteira de trabalho de {perfil["nome"]} (0 não tem): '))
+if perfil['Ctps'] != 0:
+    perfil['contratação'] = int(input(f'Ano de contratação: '))
+    anoi = year - perfil['contratação']
+    if anoi >= 35:
+        perfil['aposentadoria'] = 'aposentado'
+    else:
+        resultado = 35 - anoi
+        perfil['aposentadoria'] = resultado + idade
     perfil['salário'] = float(input(f'Salário: '))
-    for k, v in perfil.items():
-        print(f'{k} tem valor {v}')
-else:
-    for k, v in perfil.items():
-        print(f'{k} tem valor {v}')
+    print('-=' * 30)
+for k, v in perfil.items():
+    print(f'    - {k} tem valor: {v}')
