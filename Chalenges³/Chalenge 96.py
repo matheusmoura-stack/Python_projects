d7976960ce21d47c60de9fdd8f1f09116d409c5e
matheusmoura-stack <matheus.moura.g.s@gmail.com>
2022-01-20 19:15:51 -0300

    novo5

---
+++ Chalenges³/Chalenge 96.py
@@ -0,0 +1,11 @@
+def area(a, b):
+    u = a * b
+    print(f'A área do terreno {l}m por {c}m é de {u}m².')
+
+
+print('CONTROLE DE TERRENOS')
+print()
+l = float(input('largura do terreno (m): '))
+c = float(input('Comprimento do terreno (m): '))
+area(l, c)
+
